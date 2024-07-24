45fe65e143f30c6b97bb8023d8998a2659ea7257
SaiHLu <saisailuhlaing@gmail.com>
2024-07-24 20:52:52 +0630

    setup project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello 👋! I'm Wai Lin Aung.
+
+I build beautiful and performant web experiences. Also create some JS libraries as a hobby.
+
+Check more about me [here](https://wai-lin.github.io/wai-lin/).
---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: "class",
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		fontFamily: {
+			body: ["Arvo", "Noto Sans Myanmar", "serif"],
+		},
+		extend: {
+			screens: {
+				screen: { raw: "screen" },
+			},
+		},
+	},
+	plugins: [],
+}
